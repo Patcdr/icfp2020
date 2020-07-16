b43f9439c31d990c2153e498359cda0b296b4717
snydej <joshsnyder@msn.com>
2020-07-16 11:43:19 -0700

    Getting a head start :D

---
+++ framework.cpp
@@ -0,0 +1,38 @@
+                        M+M:                                                                                                                                    
+                          M~                                                                                                                                    
+                          M~                                 M                                                          M                                       
+                M  M      M~          M=                    M                                                   M        Z~                                     
+               MD MM~     M~          M=                   M                               =                +? ~M         M                                     
+              7M          M~          M=                  M=                               $,                  ~M          M                                    
+              MM        $$77$$        M=    ?O  +D=       M         ~D   :D                $,                  :M          M:                                   
+             ?M                       M=    MMM    N     $M       7   =MN $MO              $,                  :M          NM                                   
+             MM                       M=    DM     MD    MM           OM                   $,                  ~M          ?M                                   
+             MM                       M=    DM     MN    MM           MM            MMMMMMMMMMMMMMMM           ~M          :M                                   
+            MM:                       M=    DM     MN    MM           M                    $,                  ~M          IM                                   
+            MM                        M=    DM     MN    :M      +Z  MM    O               $,                  ~M          MM                                   
+            MM                        MZ    MM     MM     M      NM ~ M   M                $,                  8M=         M                                    
+           MMN                                            DD                               $,               DMMMMMNN       M                 DMMM               
+           MM                                              M                                                              M                    MM               
+          :MM                                               M                                                            N                     MM               
+          MMM                                                8                                                          M                      MM               
+          MM7                                                                                                                            M, , MMM     M  NDM  M7
+         ,MM                                                                                                                           ?M      MM    7   ?M   M 
+         MMM                       MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM        MM      MM        MM     
+         MMN                                                                                                                           MM      MM        M      
+         MM                                                                                                                            MM      MM       DM    M 
+        8MM                                                       O$                                                                    MM    8MN   MM  OM   M  
+        MMN                                                     $    M                                                                    =M8  N7~   NN   DM    
+        MM                                                     +M    MM                                  ,MMM                                                   
+        MM                                                           M:                M                   MM                                                   
+       MMN                                                          M                  M                   MM                                                   
+       MM                                           7NZM  N7N     M   :                M                   MM                                                   
+       MM                                          M   DM  MM   Z   .I$                M                   MM                                                   
+      IM+                                         D    MM      ?888888          MMMMMMMMMMMMMMMM           MM                                                   
+      MM      7MMN                                     M                               M                   MM                                                   
+      MM     M    M                                   NM    ,                          M                   MM                                                   
+     ZM     OM    NM                              MN  MM   +                           M                   MM                                                   
+     MM     MM    DM                              M  M M  M                            M                DMMMMMNN                                                
+M7  =M      MM    DM                                                                                                                                            
+MM  M       +M    MM                                                                                                                                            
+             M    M                                                                                                                                             
+               OM,                                                                                                                                               
