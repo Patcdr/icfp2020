ef6042b3d1fb1b4cfc41e324e5287b31143a96d4
Anna Bell <undefinedvalue@users.noreply.github.com>
2020-07-17 13:30:48 -0700

    Implement Interactor

---
+++ Core/IProtocol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public interface IProtocol
+    {
+        Response call(object state, object vector);
+
+        public class Response
+        {
+            public int Flag { get; set; }
+
+            public object NewState { get; set; }
+
+            public object Data { get; set; }
+        }
+    }
+}
---
+++ app/Interactor.cs
@@ -0,0 +1,36 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    class Interactor
+    {
+        public static void Interact(IProtocol protocol, object state, object vector)
+        {
+            var response = protocol.call(state, vector);
+
+            if (response.Flag == 0)
+            {
+                //   then(modem(response.NewState), multipledraw(response.Data))
+            }
+            else
+            {
+                //   else interact(protocol, Modem(response.NewState), send(response.Data))
+            }
+        }
+
+        private static object Modem(object state)
+        {
+            // What does modem do??? It looks like a noop...
+            return state;
+        }
+
+        public static async Task<int> xMain(string[] args)
+        {
+            return 0;
+        }
+    }
+}
