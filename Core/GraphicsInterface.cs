ca56918130d29cc565f6c5fcb68364ce09959e29
William Pittman <wspittman@gmail.com>
2020-07-18 10:12:52 -0700

    Core: GraphicsInterface

---
+++ Core/GraphicsInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Core
+{
+    interface GraphicsInterface
+    {
+        public Dictionary<Point, byte> AdvanceState(Point p);
+    }
+}
