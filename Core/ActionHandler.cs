e6cdcae299ad97bea03b6559fbfbceadd771604c
Chris <prichard.chris@gmail.com>
2020-07-18 20:04:24 -0700

    Interface for action handling.

---
+++ Core/ActionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Core
+{
+    public class ActionHandler
+    {
+        public void Explode(int shipId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Burn(int shipId, Point direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Laser(int shipId, Point target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Split(int shipId, int fuel, int hamburger, int cooling, int babies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
