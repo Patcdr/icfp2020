75f0f357bd8513b8d34f863e1dde4dbae6563034
Chris <prichard.chris@gmail.com>
2020-07-19 13:23:59 -0700

    Hovering DontDieRunner.

---
+++ app/DontDieRunner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Core;
+using static Core.Library;
+
+namespace app
+{
+    public class DontDieRunner : BaseRunner
+    {
+        public DontDieRunner(Sender sender, long player = 0)
+            : base(sender, player)
+        {
+        }
+
+        public override void Start()
+        {
+            Join();
+
+            if (IsDone) return;
+
+            Initialize((int)State.TotalPoints - 9, 0, 8, 1);
+
+            // Game loop
+            for (long i = State.CurrentTurn; i < State.TotalTurns; i++)
+            {
+                // Is the game over?
+                if (IsDone)
+                {
+                    return;
+                }
+
+                Ship ship = State.GetMyFirstShip();
+                
+                // Am I on a diagonal?
+                if (ship.Position.X == ship.Position.Y)
+                {
+                    int xDir = ship.Position.X > 0 ? -1 : 1;
+                    int yDir = ship.Position.Y > 0 ? -1 : 1;
+                    Command(UtilityFunctions.MakeList(Thrust(ship.ID, new Point(xDir, yDir))));
+                }
+                else
+                {
+                    // Not on a diagonal - fire in the direction where I'm closest to the planet.
+                    if (Math.Abs(ship.Position.X) > Math.Abs(ship.Position.Y))
+                    {
+                        int yDir = ship.Position.Y > 0 ? -1 : 1;
+                        Command(UtilityFunctions.MakeList(Thrust(ship.ID, new Point(0, yDir))));
+                    }
+                    else
+                    {
+                        int yDir = ship.Position.Y > 0 ? -1 : 1;
+                        Command(UtilityFunctions.MakeList(Thrust(ship.ID, new Point(0, yDir))));
+                    }
+                }
+            }
+        }
+    }
+}
