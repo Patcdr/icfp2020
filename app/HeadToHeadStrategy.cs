eccc17dcebac574c15fda9e793b9a69f0ff52907
Allan Carroll <allan@allancarroll.com>
2020-07-18 17:18:24 -0700

    Load AIs by name

--- app/HeadToHeadStrategy.cs
+++ app/HeadToHeadStrategy.cs
@@ -10,6 +10,8 @@ namespace app
 {
     class HeadToHeadStrategy : BaseInteractStrategy
     {
+        string AttackAI;
+        string DefendAI;
         GameInteractStrategy attackBot;
         GameInteractStrategy defendBot;
 
@@ -19,6 +21,8 @@ namespace app
 
         public HeadToHeadStrategy(Interactor interactor, string attackAI, string defendAI) : base(interactor)
         {
+            AttackAI = "app." + attackAI;
+            DefendAI = "app." + defendAI;
         }
 
         public HeadToHeadStrategy(Interactor interactor) : this(interactor, "GameInteractStrategy", "GameInteractStrategy")
@@ -34,8 +38,15 @@ namespace app
             var attack = players.Cdr().Car().Car().Cdr().Car();
             var defend = players.Cdr().Car().Cdr().Car().Cdr().Car();
 
-            attackBot = new GameInteractStrategy(Interactor, attack);
-            defendBot = new GameInteractStrategy(Interactor, defend);
+
+            attackBot = (GameInteractStrategy)Activator.CreateInstance(
+                Type.GetType(AttackAI),
+                new Object[] { Interactor, attack }
+            );
+            defendBot = (GameInteractStrategy)Activator.CreateInstance(
+                Type.GetType(DefendAI),
+                new Object[] { Interactor, defend }
+            );
 
             var attackExecute = Task.Factory.StartNew(() => attackBot.Start());
             var defendExecute = Task.Factory.StartNew(() => defendBot.Start());
