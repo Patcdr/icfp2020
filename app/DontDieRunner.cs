d2f902d10a57c449fb314f2033be72ed5545a219
Chris <prichard.chris@gmail.com>
2020-07-19 15:17:13 -0700

    Attempt to more actively dodge things.

--- app/DontDieRunner.cs
+++ app/DontDieRunner.cs
@@ -32,32 +32,80 @@ namespace app
                 }
 
                 Ship ship = State.GetMyFirstShip();
-                Point thrust = new Point(0, 0);
+                bool thrusted = false;
 
-                // Am I on a diagonal?
-                if (ship.Position.X == ship.Position.Y)
+                // If we would die by not thrusting
+                if (WillDieSoon(6, new Point(0, 0))) {
+                    foreach (Point p in ActionHandler.AllDirections)
+                    {
+                        // If thrusting would stop us from dying, do it!
+                        if (!WillDieSoon(6, p))
+                        {
+                            Command(Thrust(ship.ID, p));
+                            thrusted = true;
+                            break;
+                        }
+                    }
+                }
+                
+                if (!thrusted)
                 {
-                    int xDir = ship.Position.X > 0 ? -1 : 1;
+                    Command();
+                }
+            }
+        }
+
+        private bool WillDieSoon(int lookAheadTurns, Point thrust)
+        {
+            for (int i = 1; i <= lookAheadTurns; i++)
+            {
+                Point futureLocation = ShipPositionSimulator.FuturePosition(State.GetMyFirstShip(), i);
+                if (IsDeadLocation(futureLocation))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsDeadLocation(Point location)
+        {
+            return Math.Abs(location.X) <= State.StarSize ||
+                Math.Abs(location.X) >= State.ArenaSize ||
+                Math.Abs(location.Y) <= State.StarSize ||
+                Math.Abs(location.Y) >= State.ArenaSize;
+
+        }
+
+        private void HoverOnStartStrategy()
+        {
+            Ship ship = State.GetMyFirstShip();
+            Point thrust = new Point(0, 0);
+
+            // Am I on a diagonal?
+            if (ship.Position.X == ship.Position.Y)
+            {
+                int xDir = ship.Position.X > 0 ? -1 : 1;
+                int yDir = ship.Position.Y > 0 ? -1 : 1;
+                thrust = new Point(xDir, yDir);
+            }
+            else
+            {
+                // Not on a diagonal - fire in the direction where I'm furthest from the planet.
+                if (Math.Abs(ship.Position.X) < Math.Abs(ship.Position.Y))
+                {
                     int yDir = ship.Position.Y > 0 ? -1 : 1;
-                    thrust = new Point(xDir, yDir);
+                    thrust = new Point(0, yDir);
                 }
                 else
                 {
-                    // Not on a diagonal - fire in the direction where I'm furthest from the planet.
-                    if (Math.Abs(ship.Position.X) < Math.Abs(ship.Position.Y))
-                    {
-                        int yDir = ship.Position.Y > 0 ? -1 : 1;
-                        thrust = new Point(0, yDir);
-                    }
-                    else
-                    {
-                        int xDir = ship.Position.X > 0 ? -1 : 1;
-                        thrust = new Point(xDir, 0);
-                    }
+                    int xDir = ship.Position.X > 0 ? -1 : 1;
+                    thrust = new Point(xDir, 0);
                 }
-
-                Command(Thrust(ship.ID, thrust));
             }
+
+            Command(Thrust(ship.ID, thrust));
         }
     }
 }
