23e9121dcccb42539559ad5f1c7299510a182ff7
Chris <prichard.chris@gmail.com>
2020-07-18 23:54:43 -0700

    Actually commit DontDie.

---
+++ app/DontDieAI.cs
@@ -0,0 +1,58 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace app
+{
+    public class DontDieAI : BaseInteractStrategy
+    {
+        private ActionHandler handler;
+        private List<Tuple<double, double>> unitVectors;
+
+        public DontDieAI(Interactor interactor) : base(interactor)
+        {
+            // TODO: Replace null with galaxy protocol.
+            handler = new ActionHandler(new ClickInteractor(interactor, null, State));
+            unitVectors = ActionHandler.AllDirections.Select(x => ScaleToUnitLength(x)).ToList();
+        }
+
+        public override Value Next(GameState state)
+        {
+            Ship myShip = state.GetShipByPlayerId(1);
+            handler.Thrust(state, myShip.ID, GetThrustDirection(myShip.Position));
+
+            return null;
+        }
+
+        private Point GetThrustDirection(Point currentPosition)
+        {
+            Point bestDirection = new Point(0, 0);
+            double bestProduct = 0;
+            for (int i = 0; i < ActionHandler.AllDirections.Count; i++)
+            {
+                double product = unitVectors[i].Item1 * currentPosition.X
+                    + unitVectors[i].Item2 + currentPosition.Y;
+                if (product > bestProduct)
+                {
+                    bestDirection = ActionHandler.AllDirections[i];
+                    bestProduct = product;
+                }
+            }
+
+            return new Point(-1 * bestDirection.X, -1 * bestDirection.Y);
+        }
+
+        private Tuple<double, double> ScaleToUnitLength(Point p)
+        {
+            double length = GetLength(p);
+            return new Tuple<double, double>(p.X / length, p.Y / length);
+        }
+
+        private double GetLength(Point p)
+        {
+            return Math.Sqrt(p.X * p.X + p.Y * p.Y);
+        }
+    }
+}
