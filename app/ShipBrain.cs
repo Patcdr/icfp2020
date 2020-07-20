24bf2ed92c5358f13d6a990182691bb9d74135e4
snydej <joshsnyder@msn.com>
2020-07-19 23:06:47 -0700

    Brainz

---
+++ app/ShipBrain.cs
@@ -0,0 +1,22 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app
+{
+    interface ShipBrain
+    {
+        // ship is this ship (Use this instead of state.GetMyFirstShip())
+        // state is the GameState this turn
+        //
+        // Returns the commands you want this ship to use this turn
+        //
+        // If you would normally do this:
+        // Command(Thrust(shipId, vec));
+        //
+        // You can do this instead:
+        // yield return Thrust(shipId, vec)
+        public IEnumerable<Value> Step(Ship ship, GameState state);
+    }
+}
---
+++ app/SnyderRunner.cs
@@ -0,0 +1,42 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace app
+{
+    class SnyderRunner : BaseRunner
+    {
+
+        (int lazers, int cooling, int ships) initialStats;
+        Dictionary<long, ShipBrain> brains;
+        public SnyderRunner(
+            Sender sender,
+            ShipBrain initialBrain,
+            (int lazers, int cooling, int ships) initialStats,
+            long player = 0)
+            : base(sender, player)
+        {
+        }
+
+        protected override (int lazers, int cooling, int ships) GetInitialValues(bool isAttacker)
+        {
+            return initialStats;
+        }
+
+        public override void Step()
+        {
+            if (IsDone) return;
+
+            List<Value> commands = new List<Value>();
+
+            foreach (Ship ship in State.Ships)
+            {
+                commands.AddRange(brains[ship.ID].Step(ship, State));
+            }
+
+            Command(commands.ToArray());
+        }
+    }
+}
