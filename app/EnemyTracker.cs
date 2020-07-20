e31b21e73cbde4b5f2131aca248fa74324e4d81b
snydej <joshsnyder@msn.com>
2020-07-20 00:16:06 -0700

    EnemyTracker skeleton

---
+++ app/EnemyTracker.cs
@@ -0,0 +1,32 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace app
+{
+    class EnemyTracker
+    {
+        public EnemyTracker()
+        {
+
+        }
+
+        // This needs to be called every step with the GameState
+        public void GatherIntel(GameState state)
+        {
+            
+        }
+
+        public Point Predict(long shipId, int numTurns)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumTurnsCorrectlyPredicted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
